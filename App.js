0667c8685180ea33fe0c01c6c6ca72042e37b6c3
Thiago Silva Azevedo <thiago.dev23@gmail.com>
2025-04-03 10:30:24 -0300

    update images

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, FlatList, StyleSheet, Text, StatusBar, Image } from 'react-native';
+import { View, FlatList, StyleSheet, Text, Image } from 'react-native';
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
 
 
@@ -8,22 +8,22 @@ const DATA = [
   
   {
     id: '1',
-    image: 'https://i.pinimg.com/736x/af/41/fc/af41fc65f1fa2c145ff00106568afa39.jpg',
+    image: 'https://fla-image.mundobola.com/_next/image/?url=https%3A%2F%2Ffla-media.mundobola.com%2Fmedia%2F2023%2F12%2FMundial-1981.webp&w=3840&q=75',
     text: ["Mundial: 1x", "Anos: 1981", "Finais: Flamengo 3x0 Liverpool"],
   },
   {
     id: '2',
-    image: 'https://i.pinimg.com/736x/26/3e/09/263e09a8b6342b65bb8a8bd1f5dbfec5.jpg',
+    image: 'https://a.espncdn.com/combiner/i?img=%2Fphoto%2F2019%2F1124%2Fr632336_1296x729_16%2D9.jpg',
     text: ["Copa Libertadores: 3x", "Anos: 1981 2019 2022", "Finais:", "Flamengo 4x2(Ag) Cobreloa", "Flamengo 2x1 River Plate", "Flamengo 1x0 Athletico Paranaense"],
   },
   {
     id: '3',
-    image: 'https://media.gettyimages.com/id/1434873773/pt/foto/rio-de-janeiro-brazil-the-trophy-is-displayed-prior-to-the-second-leg-match-of-the-final-of.jpg?s=612x612&w=0&k=20&c=f4sSzU1nBTWxv2fk1ByFQzkoyiJZoPyLNeqQZNmuobk=',
+    image: 'https://assets.goal.com/images/v3/blt48df22d6907bc36c/GettyImages-1434896261.jpg?auto=webp&format=pjpg&width=3840&quality=60',
     text: ["Copa do Brasil: 5x", "Anos: 1990 2006 2013 2022 2024", "Finais:", "Flamengo 1x0(Ag) Goiás", "Flamengo 3x0(Ag) Vasco", "Flamengo 3x1(Ag) Athletico Paranaense", "Flamengo (P6)1x1(P5)(AG) Corinthians", "Flamengo 4x1(AG) Athletico Mineiro"],
   },
   {
     id: '4',
-    image: 'https://i.pinimg.com/736x/b3/3a/00/b33a0061f9cb893876fbf32b98520bfc.jpg',
+    image: 'https://s2.glbimg.com/d63FujljV1mgqziMafd--g-eeQM=/640x424/i.glbimg.com/og/ig/infoglobo1/f/original/2019/12/05/36093669_es_06-12-2009_rio_de_janeiro_rj_final_do_campeonato_brasileiro_2009_flamengo_x_gremio_no_ma.jpg',
     text: ["Brasileirão: 8x", "Anos:1980 1982 1983 1987 1992 2009 2019 2020"],
   },
 ];
@@ -71,7 +71,7 @@ const styles = StyleSheet.create({
   image: {
     width: "100%",
     height: 220,
-    resizeMode: 'contain',
+
     borderRadius: 10,
     marginTop: 0,
     
